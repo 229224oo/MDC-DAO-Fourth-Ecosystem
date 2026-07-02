c8bbe68c5a61ba08a430db0a825c404006213854
229224oo <547718270@qq.com>
2026-07-02 12:46:26 +0800

    文件更新

---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const root = __dirname;
+const port = 8080;
+const types = {
+  '.html': 'text/html; charset=utf-8',
+  '.js': 'text/javascript; charset=utf-8',
+  '.css': 'text/css; charset=utf-8',
+  '.svg': 'image/svg+xml',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.ico': 'image/x-icon',
+  '.json': 'application/json; charset=utf-8',
+};
+
+http.createServer((req, res) => {
+  let urlPath = decodeURIComponent(req.url.split('?')[0]);
+  if (urlPath === '/') urlPath = '/index.html';
+  const filePath = path.join(root, urlPath);
+  if (!filePath.startsWith(root)) {
+    res.writeHead(403);
+    return res.end('Forbidden');
+  }
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
+      return res.end('404 Not Found');
+    }
+    res.writeHead(200, { 'Content-Type': types[path.extname(filePath)] || 'application/octet-stream' });
+    res.end(data);
+  });
+}).listen(port, '127.0.0.1', () => {
+  console.log('Server running at http://127.0.0.1:' + port + '/');
+});
